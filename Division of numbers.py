45aecd5a41091ebe7ae740d74271991f1c4e5465
Mahoro752 <61754973+Mahoro752@users.noreply.github.com>
2020-03-09 20:22:52 +0300

    Add files via upload

---
+++ Division of numbers.py
@@ -0,0 +1,5 @@
+first_number = int(input("Enter First Number: "))
+second_number = int(input("Enter Second Number: "))
+
+A = first_number / second_number
+print("The Divison of first and second number is: \n", round(A,2))
